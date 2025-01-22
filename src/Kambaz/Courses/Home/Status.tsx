4b7ee50dff15b9ca0f8f4306bb397a1a64336361
tishakotadia <tisha.kotadia@hotmail.com>
2025-01-22 17:45:58 -0500

    course status styling

--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -1,13 +1,19 @@
 export default function CourseStatus() {
     return (
-    <div id="wd-course-status">
-    <h2>Course Status</h2>
-    <button>Unpublish</button> <button>Publish</button>
-    <button>Import Existing Content</button>
-    <button>Import from Commons</button>
-    <button>Choose Home Page</button>
-    <button>View Course Stream</button>
-    <button>New Announcement</button>
-    <button>New Analytics</button>
-    <button>View Course Notifications</button>
-</div> );}
+      <div id="wd-course-status">
+        <h2>Course Status</h2>
+        <div style={{ display: "flex", alignItems: "center" }}>
+            <button style={{ marginRight: "10px" }}>Unpublish</button>
+            <button style={{ marginRight: "10px" }}>Publish</button>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", marginTop: "10px" }}>
+            <button>Import Existing Content</button>
+            <button>Import from Commons</button>
+            <button>Choose from Homepage</button>
+            <button>View Course Stream</button>
+            <button>New Announcement</button>
+            <button>New Analytics</button>
+            <button>View Course Notifications</button>
+        </div> 
+    </div>
+  );}
