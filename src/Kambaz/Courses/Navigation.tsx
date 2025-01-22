addf7d06d284a3431c1d4820707033bc30638cd9
tishakotadia <tisha.kotadia@hotmail.com>
2025-01-22 18:28:28 -0500

    Headings for empty pages

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -10,7 +10,7 @@ export default function CourseNavigation() {
         <li><Link to="/Kambaz/Courses/1234/Assignments" id="wd-course-quizzes-link"> Assignments</Link><br/> </li>
         <li><Link to="/Kambaz/Courses/1234/Quizzes" id="wd-course-assignments-link">Quizzes </Link><br/> </li>
         <li><Link to="/Kambaz/Courses/1234/Grades" id="wd-course-grades-link">Grades</Link><br/> </li>
-        <li><Link to="/Kambaz/People" id="wd-course-people-link">People</Link><br/> </li>
+        <li><Link to="/Kambaz/Courses/1234/People" id="wd-course-people-link">People</Link><br/> </li>
     </ul> 
     </div> 
     ); 
