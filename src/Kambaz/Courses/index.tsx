c03db2d85d95e66ef1666b03ec2e61f67da857c5
tishakotadia <tisha.kotadia@hotmail.com>
2025-01-22 11:05:56 -0500

    Implementing the Kambaz Course Home Screen

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,5 +1,6 @@
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
+import Home from "./Home";
 import { Navigate, Route, Routes } from "react-router";
 export default function Courses() {
     return (
@@ -14,7 +15,7 @@ export default function Courses() {
                 <td valign="top"> 
                     <Routes> 
                         <Route path="/" element={<Navigate to="Home" />} /> 
-                        <Route path="Home" element={<h2>Home</h2>} /> 
+                        <Route path="Home" element={<Home />} /> 
                         <Route path="Modules" element={<Modules />} /> 
                         <Route path="Assignments" element={<h2>Assignments</h2>} /> 
                         <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} /> 
