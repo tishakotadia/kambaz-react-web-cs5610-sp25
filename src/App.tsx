74cfa9a1a6f814caaf707d2bf643055bcafcc3d1
tishakotadia <tisha.kotadia@hotmail.com>
2025-01-22 02:47:21 -0500

    Implementing the Kambaz Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import Labs from "./Labs";
+import Labs from "./Labs"; 
+import Kambaz from "./Kambaz";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
-  return ( 
-    <div> 
-      <Labs /> 
-    </div>
-);}
+  return (
+    <HashRouter>
+      <div>
+        <Routes> <Route path="/" element={<Navigate to="Kambaz"/>}/>
+        <Route path="/Labs/*" element={<Labs />} />
+        <Route path="/Kambaz/*" element={<Kambaz />} />
+        </Routes>
+      </div>
+    </HashRouter> );}
