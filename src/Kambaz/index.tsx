546edc9303b939fc13fcd03f7340cd85e8bd3e32
tishakotadia <tisha.kotadia@hotmail.com>
2025-01-22 09:03:52 -0500

    Navigation for Kambaz signin, signup and profile

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,6 +1,12 @@
+import { Routes, Route, Navigate } from "react-router"; 
+import Account from "./Account";
 export default function Kambaz() {
     return (
     <div id="wd-kambaz">
-    <h1>Kambaz</h1>
+        <h1>Kambaz</h1>
+        <Routes> 
+            <Route path="/" element={<Navigate to="Account" />} /> 
+            <Route path="/Account/*" element={<Account />} /> 
+        </Routes>
     </div>
-    );}
+);}
