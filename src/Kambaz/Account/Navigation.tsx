c146d6a42520634542d267c5011d49cfdc6573b7
tishakotadia <tisha.kotadia@hotmail.com>
2025-01-22 18:03:54 -0500

    unordered lists in navigation

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom";
 export default function AccountNavigation() {
 return (
     <div id="wd-account-navigation">
-        <Link to={`/Kambaz/Account/Signin`} > Signin </Link> <br/>
-        <Link to={`/Kambaz/Account/Signup`} > Signup </Link> <br/>
-        <Link to={`/Kambaz/Account/Profile`} > Profile </Link> <br/>
+    <ul>
+        <li><Link to={`/Kambaz/Account/Signin`} > Signin </Link> <br/></li>
+        <li><Link to={`/Kambaz/Account/Signup`} > Signup </Link> <br/></li>
+        <li><Link to={`/Kambaz/Account/Profile`} > Profile </Link> <br/></li>
+    </ul> 
     </div>
 );}
