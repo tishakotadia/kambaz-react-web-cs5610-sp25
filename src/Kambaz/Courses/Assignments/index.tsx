cfd68029de485e2a9effd033e40bee448ac3b3e5
tishakotadia <tisha.kotadia@hotmail.com>
2025-01-22 17:00:09 -0500

    break added in assignments

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -10,7 +10,7 @@ export default function Assignments() {
             <a href="#/Kambaz/Courses/1234/Assignments/123" className="wd-assignment-link" >
             A1 - ENV + HTML
             </a> 
-            <p>Multiple Modules | <strong>Not available until </strong> May 6 at 12:00am | 
+            <p>Multiple Modules | <strong>Not available until </strong> May 6 at 12:00am | <br />
             <strong>Due </strong>May 13 at 11:59pm | 100 pts
             </p></li>
 
@@ -18,7 +18,7 @@ export default function Assignments() {
             <a href="#/Kambaz/Courses/1234/Assignments/123" className="wd-assignment-link" >
             A2 - CSS + BOOTSTRAP
             </a> 
-            <p>Multiple Modules | <strong>Not available until </strong> May 13 at 12:00am | 
+            <p>Multiple Modules | <strong>Not available until </strong> May 13 at 12:00am | <br />
             <strong>Due </strong>May 20 at 11:59pm | 100 pts
             </p></li>
 
@@ -26,7 +26,7 @@ export default function Assignments() {
             <a href="#/Kambaz/Courses/1234/Assignments/123" className="wd-assignment-link" >
             A3 - JAVASCRIPT + REACT
             </a> 
-            <p>Multiple Modules | <strong>Not available until </strong> May 20 at 12:00am | 
+            <p>Multiple Modules | <strong>Not available until </strong> May 20 at 12:00am | <br />
             <strong>Due </strong>May 27 at 11:59pm | 100 pts
             </p></li>
 
