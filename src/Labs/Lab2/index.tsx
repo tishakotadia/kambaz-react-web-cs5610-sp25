ec8ef8f594ad5b7bdde8c3300f793c34c62341bb
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-04 04:42:05 -0500

    import css file

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,10 +1,10 @@
+import "./index.css";
 export default function Lab2() {
     return (
     <div id="wd-lab2">
     <h2>Lab 2 - Cascading Style Sheets</h2>
     <h3>Styling with the STYLE attribute</h3>
-    <p style={{ backgroundColor: "blue",
-    color: "white" }}>
+    <p>
     Style attribute allows configuring look and feel
     right on the element. Although it's very convenient
     it is considered bad practice and you should avoid
