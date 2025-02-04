0e273a537860a1e60a0f059d7e5bf322d12e9d7e
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-03 19:28:45 -0500

    Correction-Adding multiple lessons in modules

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -10,7 +10,7 @@ export default function Modules() {
         <button style={{ marginRight: "10px" }}>+ Module</button>
         <ul id="wd-modules">
           <li className="wd-module">
-            <div className="wd-title">Week 1</div>
+            <div className="wd-title">Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda</div>
             <ul className="wd-lessons">
               <li className="wd-lesson">
                 <span className="wd-title">LEARNING OBJECTIVES</span>
@@ -19,28 +19,58 @@ export default function Modules() {
                   <li className="wd-content-item">Learn what is Web Development</li>
                 </ul>
               </li>
-            </ul>
-          </li>
-          <li className="wd-module">
-            <div className="wd-title">Week 2</div>
-            <ul className="wd-lessons">
               <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
+                <span className="wd-title">READING</span>
                 <ul className="wd-content">
-                  <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
-                  <li className="wd-content-item">Keep working on assignment 1</li>
+                  <li className="wd-content-item">Full Stack Developer - Chapter 1 - Introduction</li>
+                  <li className="wd-content-item">Full Stack Developer - Chapter 2 - Creating User Interface</li>
+                </ul>
+              </li>
+              <li className="wd-lesson">
+                <span className="wd-title">SLIDES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Introduction to Web Development</li>
+                  <li className="wd-content-item">Creating an HTTP server with Node.js</li>
+                  <li className="wd-content-item">Creating a React Application</li>
                 </ul>
               </li>
             </ul>
           </li>
           <li className="wd-module">
-            <div className="wd-title">Week 3</div>
+            <div className="wd-title">Week 2, Lecture 2 - Formatting User Interfaces with HTML</div>
+            <ul className="wd-lessons">
+              <li className="wd-lesson">
+                <span className="wd-title">LEARNING OBJECTIVES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
+                  <li className="wd-content-item">Deploy the assignment to Netlify</li>
+                </ul>
+              </li>
+              <li className="wd-lesson">
+                <span className="wd-title">SLIDES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Introduction to HTML and DOM</li>
+                  <li className="wd-content-item">Formatting Web content with Headings</li>
+                  <li className="wd-content-item">Formatting Web content with Lists and Tables</li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li className="wd-module">
+            <div className="wd-title">Week 3, Lecture 3 - Formatting User Interfaces with CSS</div>
             <ul className="wd-lessons">
               <li className="wd-lesson">
                 <span className="wd-title">LEARNING OBJECTIVES</span>
                 <ul className="wd-content">
                   <li className="wd-content-item">Introduction to CSS</li>
                   <li className="wd-content-item">Selectors by tag ID, classes, and document structure</li>
+                </ul>
+              </li>
+              <li className="wd-lesson">
+                <span className="wd-title">SLIDES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Introduction to CSS and Flex</li>
+                  <li className="wd-content-item">Formatting Web content with CSS styling</li>
                 </ul>
               </li>
             </ul>
