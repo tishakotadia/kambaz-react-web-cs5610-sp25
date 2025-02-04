665f2957a6cdfdea2488a8e2e51c051089a2450d
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-04 04:49:10 -0500

    class selector

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -13,5 +13,18 @@ export default function Lab2() {
     <div id="wd-css-id-selectors"> <h3>ID selectors</h3> <p id="wd-id-selector-1"> 
         Instead of changing the look and feel of all the elements of the same name, e.g., P, we can refer to a 
         specific element by its ID </p> <p id="wd-id-selector-2"> Here's another paragraph using a different ID
-         and a different look and feel </p> </div>
+         and a different look and feel </p> 
+    </div>
+
+    <div id="wd-css-class-selectors">
+    <h3>Class selectors</h3>
+    <p className="wd-class-selector">
+    Instead of using IDs to refer to elements, you can use an element's CLASS attribute
+    </p>
+    <h4 className="wd-class-selector">
+    This heading has same style as paragraph above
+    </h4>
+    </div>
+
+    
     </div>);}
