807f6275f1555e05982ba55c3f47a6a19ac66c19
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-05 15:17:04 -0500

    2.4.6 done

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -5,6 +5,7 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import { Navigate, Route, Routes } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
+import PeopleTable from "./People/Table";
 export default function Courses() {
     return (
     <div id="wd-courses">
@@ -23,7 +24,7 @@ export default function Courses() {
                         <Route path="Assignments/:aid" element={<AssignmentEditor />} /> 
                         <Route path="Quizzes" element={<h2>Quizzes</h2>} />
                         <Route path="Grades" element={<h2>Grades</h2>} />
-                        <Route path="People" element={<h2>People</h2>} /> 
+                        <Route path="People" element={<PeopleTable />} /> 
                     </Routes> 
                     </div></div>
     </div>
