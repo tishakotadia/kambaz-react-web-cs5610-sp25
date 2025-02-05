1975e794b8e32f20eb22e3fda2d3fc93d3c76b60
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-05 11:05:02 -0500

    2.4.1 done

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -3,12 +3,13 @@ import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KambazNavigation from "./Navigation";
 import Courses from "./Courses";
+import "./styles.css";
 export default function Kambaz() {
     return (
     <div id="wd-kambaz">
          
                     <KambazNavigation /> 
-                <div>
+                <div className="wd-main-content-offset p-3">
                     <Routes> 
                         <Route path="/" element={<Navigate to="Account" />} /> 
                         <Route path="/Account/*" element={<Account />} /> 
