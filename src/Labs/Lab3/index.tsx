25c775427ae2bcb89c718fa61546b5d822ef26bd
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-15 15:27:57 -0500

    lab 3.1 done

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,6 +1,21 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
 export default function Lab3() {
     return (
-    <div>
-    <h2>Lab 3</h2>
+    <div id="wd-lab3">
+    <h3>Lab 3</h3>
+    <VariablesAndConstants/>
+    <VariableTypes/>
+    <BooleanVariables/>
+    <IfElse/>
+    <TernaryOperator/>
+    <ConditionalOutputIfElse/>
+    <ConditionalOutputInline/>
     </div>
-);}
+    );
+    }
