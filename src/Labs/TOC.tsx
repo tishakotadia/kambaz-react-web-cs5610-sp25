1469e1334239c96fcd824065d8fc45fa9aa306c4
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-18 09:39:02 -0500

    3.5.2 done

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -13,25 +13,41 @@
 //     );
 // }
 
+//<Nav.Item> 
+//    <Nav.Link href="#/Labs">Default Lab</Nav.Link> 
+//</Nav.Item>
+
 import Nav from "react-bootstrap/Nav";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 export default function TOC() {
+    const { pathname } = useLocation();
      return (
-        <Nav variant="pills"> <Nav.Item> 
-            <Nav.Link href="#/Labs">Default Lab</Nav.Link> 
-            </Nav.Item> 
-            <Nav.Item> 
-                <Nav.Link href="#/Labs/Lab1">Lab 1</Nav.Link>
-                </Nav.Item> 
-                <Nav.Item> 
-                    <Nav.Link href="#/Labs/Lab2">Lab 2</Nav.Link> 
-                    </Nav.Item> <Nav.Item> 
-                        <Nav.Link href="#/Labs/Lab3">Lab 3</Nav.Link> 
-                        </Nav.Item> <Nav.Item> 
-                            <Nav.Link href="#/Kambaz">Kambaz</Nav.Link> 
-                            </Nav.Item> <Nav.Item> 
-                                <Nav.Link href="https://github.com/tishakotadia/kambaz-react-web-cs5610-sp25" 
+        <Nav variant="pills" id="wd-toc">  
+
+        <Nav.Item> 
+            <Nav.Link as={Link} to="/Labs/Lab1" id="wd-a1" active={pathname.includes("Lab1")}>Lab 1</Nav.Link>
+        </Nav.Item> 
+                
+        <Nav.Item> 
+            <Nav.Link as={Link} to="/Labs/Lab2" id="wd-a2" active={pathname.includes("Lab2")}>Lab 2</Nav.Link> 
+        </Nav.Item> 
+
+        <Nav.Item> 
+            <Nav.Link as={Link} to="/Labs/Lab3" id="wd-a3" active={pathname.includes("Lab3")}>Lab 3</Nav.Link> 
+        </Nav.Item> 
+        
+        <Nav.Item> 
+            <Nav.Link as={Link} to="/Kambaz">Kambaz</Nav.Link> 
+        </Nav.Item> 
+        
+        <Nav.Item> 
+            <Nav.Link href="https://github.com/tishakotadia/kambaz-react-web-cs5610-sp25" 
           id="wd-github"
           target="_blank"
-          rel="noopener noreferrer">My GitHub</Nav.Link> 
-                                </Nav.Item> </Nav> 
+          rel="noopener noreferrer"
+          active={false}>My GitHub</Nav.Link> 
+        </Nav.Item> 
+        
+        </Nav> 
             );}
