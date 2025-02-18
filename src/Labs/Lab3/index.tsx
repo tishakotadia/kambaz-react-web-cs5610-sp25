6cf15d768c442d0353f468d25bfb41cab37cd936
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-18 06:31:02 -0500

    3.3.13 done

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -13,8 +13,17 @@ import SimpleArrays from "./SimpleArrays";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
 import ForLoops from "./ForLoops";
-
-
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
 export default function Lab3() {
     return (
     <div id="wd-lab3">
@@ -34,6 +43,17 @@ export default function Lab3() {
     <ArrayIndexAndLength/>
     <AddingAndRemovingToFromArrays/>
     <ForLoops/>
+    <MapFunction/>
+    <FindFunction/>
+    <FindIndex/>
+    <FilterFunction/>
+    <JsonStringify/>
+    <House/>
+    <TodoItem/>
+    <TodoList/>
+    <Spreading/>
+    <Destructing/>
+    <FunctionDestructing/>
     </div>
     );
     }
