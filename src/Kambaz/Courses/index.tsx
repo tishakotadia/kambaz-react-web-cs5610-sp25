66ca11de82a8b24d6307a51d2170918cffffe30c
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-18 18:47:26 -0500

    3.7.4 done

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -3,13 +3,16 @@ import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
-import { Navigate, Route, Routes } from "react-router";
-import { FaAlignJustify } from "react-icons/fa";
+import { Navigate, Route, Routes, useParams } from "react-router";
+import { courses } from "../Database";
+import { FaAlignJustify } from "react-icons/fa6";
 import PeopleTable from "./People/Table";
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
     return (
     <div id="wd-courses">
-        <h2 className="text-danger"> <FaAlignJustify className="me-4 fs-4 mb-1" />Course 1234</h2>
+        <h2 className="text-danger"> <FaAlignJustify className="me-4 fs-4 mb-1" />{course && course.name}</h2>
         <hr /> 
         <div className="d-flex"> <div className="d-none d-md-block">
                     <CourseNavigation /> 
