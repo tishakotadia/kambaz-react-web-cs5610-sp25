6220efc9082fddf3b859d700d2407bdde45a7a6e
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-19 16:27:20 -0500

    name change in lab1

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,6 +2,8 @@ export default function Lab1() {
     return (
         <div id="wd-lab1">
             <h2>Lab 1</h2>
+            <h2>Tisha Sujal Kotadia</h2>
+            <h2>Section 01</h2>
             <h3>HTML Examples</h3> 
 
             <div id="wd-h-tag"> 
