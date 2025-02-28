2a964478bb3ba02e162c294c12cb8330204c26e9
tishakotadia <tisha.kotadia@hotmail.com>
2025-02-28 09:59:11 -0500

    lab 4.2 done

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -2,6 +2,13 @@ import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
 import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 export default function Lab4() {
     function sayHello() {
         alert("Hello");
@@ -14,5 +21,13 @@ export default function Lab4() {
         <PassingDataOnEvent />
         <PassingFunctions theFunction={sayHello} />
         <EventObject />
+        <Counter />
+        <BooleanStateVariables />
+        <StringStateVariables />
+        <DateStateVariable />
+        <ObjectStateVariable />
+        <ArrayStateVariable />
+        <hr />
+        <ParentStateComponent />
         </div>);
 }
