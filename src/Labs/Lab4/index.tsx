ae5e24034f87f467fd84fb08360b6c3aea1dd45b
tishakotadia <tisha.kotadia@hotmail.com>
2025-03-01 14:42:25 -0500

    redux installed

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -9,6 +9,7 @@ import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 export default function Lab4() {
     function sayHello() {
         alert("Hello");
@@ -29,5 +30,6 @@ export default function Lab4() {
         <ArrayStateVariable />
         <hr />
         <ParentStateComponent />
+        <ReduxExamples />
         </div>);
 }
