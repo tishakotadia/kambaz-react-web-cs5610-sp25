9ffb9d66abc6037d919d7862844c0ee72f443a8b
tishakotadia <tisha.kotadia@hotmail.com>
2025-03-04 19:10:00 -0500

    added components of 2,3,4 in ReduxExamples index file

--- src/Labs/Lab4/ReduxExamples/index.tsx
+++ src/Labs/Lab4/ReduxExamples/index.tsx
@@ -1,9 +1,14 @@
 //import React from "react";
-
+import HelloRedux from "./HelloRedux";
+import CounterRedux from "./CounterRedux";
+import AddRedux from "./AddRedux";
 export default function ReduxExamples() {
   return(
     <div>
       <h2>Redux Examples</h2>
+      <HelloRedux />
+      <CounterRedux />
+      <AddRedux />
     </div>
   );
 };
