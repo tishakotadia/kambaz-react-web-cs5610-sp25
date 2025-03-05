807b2c5c1589e6fdd41f1d657c6d679895f56796
tishakotadia <tisha.kotadia@hotmail.com>
2025-03-04 19:02:09 -0500

    4.3.4 done

--- src/Labs/store/index.tsx
+++ src/Labs/store/index.tsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../Lab4/ReduxExamples/HelloRedux/helloReducer";
 import counterReducer from "../Lab4/ReduxExamples/CounterRedux/counterReducer";
+import addReducer     from "../Lab4/ReduxExamples/AddRedux/addReducer";
 const store = configureStore({
   reducer: { helloReducer,
-    counterReducer
+    counterReducer,
+    addReducer
    }});
 export default store;
