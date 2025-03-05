7e106f182a1c06f629436abeb9ec39e9734fef09
tishakotadia <tisha.kotadia@hotmail.com>
2025-03-05 16:04:12 -0500

    kambaz 4.4.2 done

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -4,10 +4,10 @@ import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
-import { courses } from "../Database";
+//import { courses } from "../Database";
 import { FaAlignJustify } from "react-icons/fa6";
 import PeopleTable from "./People/Table";
-export default function Courses() {
+export default function Courses({ courses }: { courses: any[]; }) {
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
   const { pathname } = useLocation();
--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,34 +1,35 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import * as db from "./Database";
 import { Row, Col, Card, Button, FormControl } from "react-bootstrap"; 
-import { v4 as uuidv4 } from "uuid";
 
-export default function Dashboard() { 
-    const [courses, setCourses] = useState<any[]>(db.courses);
-    const [course, setCourse] = useState<any>({
-        _id: "0", name: "New Course", number: "New Number",
-        startDate: "2023-09-10", endDate: "2023-12-15",
-        image: "/images/reactjs.jpg", description: "New Description"
-      });
-      const addNewCourse = () => {
-        const newCourse = { ...course, _id: uuidv4() };
-        setCourses([...courses, newCourse ]);
-      };
-      const deleteCourse = (courseId: string) => {
-        setCourses(courses.filter((course) => course._id !== courseId));
-      };
-      const updateCourse = () => {
-        setCourses(
-          courses.map((c) => {
-            if (c._id === course._id) {
-              return course;
-            } else {
-              return c;
-            }
-          })
-        );
-      };
+export default function Dashboard({ courses, course, setCourse, addNewCourse,
+    deleteCourse, updateCourse }: {
+    courses: any[]; course: any; setCourse: (course: any) => void;
+    addNewCourse: () => void; deleteCourse: (course: any) => void;
+    updateCourse: () => void; }) { 
+    // const [courses, setCourses] = useState<any[]>(db.courses);
+    // const [course, setCourse] = useState<any>({
+    //     _id: "0", name: "New Course", number: "New Number",
+    //     startDate: "2023-09-10", endDate: "2023-12-15",
+    //     image: "/images/reactjs.jpg", description: "New Description"
+    //   });
+    //   const addNewCourse = () => {
+    //     const newCourse = { ...course, _id: uuidv4() };
+    //     setCourses([...courses, newCourse ]);
+    //   };
+    //   const deleteCourse = (courseId: string) => {
+    //     setCourses(courses.filter((course) => course._id !== courseId));
+    //   };
+    //   const updateCourse = () => {
+    //     setCourses(
+    //       courses.map((c) => {
+    //         if (c._id === course._id) {
+    //           return course;
+    //         } else {
+    //           return c;
+    //         }
+    //       })
+    //     );
+    //   };
     
     
     return ( 
