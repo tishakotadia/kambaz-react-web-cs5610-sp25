4aa9bcda661d08c48c3ff86a73d0ae7babefc656
tishakotadia <tisha.kotadia@hotmail.com>
2025-03-06 12:56:16 -0500

    kambaz 4.4.3 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Labs from "./Labs"; 
 import Kambaz from "./Kambaz";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import store from "./Kambaz/store";
+import { Provider } from "react-redux";
 export default function App() {
   return (
     <HashRouter>
+      <Provider store={store}>
       <div>
         <Routes> <Route path="/" element={<Navigate to="Kambaz"/>}/>
         <Route path="/Labs/*" element={<Labs />} />
         <Route path="/Kambaz/*" element={<Kambaz />} />
         </Routes>
       </div>
+      </Provider>
     </HashRouter> );}
