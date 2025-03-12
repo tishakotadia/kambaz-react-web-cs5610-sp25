4201096c6961289728b408f45e29218e238b2892
tishakotadia <tisha.kotadia@hotmail.com>
2025-03-12 17:59:43 -0400

    build error

--- src/Labs/Lab4/ReduxExamples/index.tsx
+++ src/Labs/Lab4/ReduxExamples/index.tsx
@@ -2,7 +2,7 @@
 import HelloRedux from "./HelloRedux";
 import CounterRedux from "./CounterRedux";
 import AddRedux from "./AddRedux";
-import TodoList from "./todos/ToDoList";
+import TodoList from "./todos/TodoList";
 export default function ReduxExamples() {
   return(
     <div>
