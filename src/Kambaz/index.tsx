da8fbcd52f9d861d270161b8c175e8ccc053dfb3
tishakotadia <tisha.kotadia@hotmail.com>
2025-03-12 23:50:42 -0400

    last change a4

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -3,40 +3,14 @@ import Account from "./Account/index";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import KambazNavigation from "./Navigation";
-import "./styles.css"
-import * as db from "./Database";
-import { useState } from "react";
-// import { v4 as uuidv4 } from "uuid";
+import "./styles.css";
+import { useSelector } from "react-redux"; // ✅ Import Redux state
 import ProtectedRoute from "./Account/ProtectedRoute";
+
 export default function Kambaz() {
-  const [courses] = useState<any[]>(db.courses);
-  // const [course, setCourse] = useState<any>({
-  //   _id: "0",
-  //   name: "New Course",
-  //   number: "New Number",
-  //   startDate: "2023-09-10",
-  //   endDate: "2023-12-15",
-  //   image: "/images/react.png",
-  //   description: "New Description",
-  // });
+  // ✅ Get courses dynamically from Redux
+  const courses = useSelector((state: any) => state.coursesReduccer.courses);
 
-  // Add new course
-  // const addNewCourse = () => {
-  //   const newCourse = { ...course, _id: uuidv4() };
-  //   setCourses([...courses, newCourse]);
-  // };
-
-  // // Delete course
-  // const deleteCourse = (courseId: string) => {
-  //   setCourses(courses.filter((course) => course._id !== courseId));
-  // };
-
-  // // Update course
-  // const updateCourse = () => {
-  //   setCourses(
-  //     courses.map((c) => (c._id === course._id ? course : c))
-  //   );
-  // };
   return (
     <div id="wd-kambaz">
       <KambazNavigation />
@@ -44,21 +18,25 @@ export default function Kambaz() {
         <Routes>
           <Route path="/" element={<Navigate to="/Kambaz/Account" />} />
           <Route path="/Account/*" element={<Account />} />
-          <Route path="/Dashboard" element={ <ProtectedRoute>
-            <Dashboard
-            // courses={courses}
-            // course={course}
-            // setCourse={setCourse}
-            // addNewCourse={addNewCourse}
-            // deleteCourse={deleteCourse}
-            // updateCourse={updateCourse}
-            />
+          
+          {/* ✅ Dashboard is now fully protected */}
+          <Route path="/Dashboard" element={
+            <ProtectedRoute>
+              <Dashboard />
             </ProtectedRoute>
-            } />
-          <Route path="/Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses}/> </ProtectedRoute>} />
+          } />
+
+          {/* ✅ Courses now fetches courses from Redux automatically */}
+          <Route path="/Courses/:cid/*" element={
+            <ProtectedRoute>
+              <Courses />
+            </ProtectedRoute>
+          } />
+
           <Route path="/Calendar" element={<h1>Calendar</h1>} />
           <Route path="/Inbox" element={<h1>Inbox</h1>} />
         </Routes>
       </div>   
     </div>
-);}
+  );
+}
