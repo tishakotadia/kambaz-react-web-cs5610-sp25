bb0388ce1bfcd02234c6be9fcc56e07d72a95e62
tishakotadia <tisha.kotadia@hotmail.com>
2025-03-12 23:55:41 -0400

    a4 f

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -3,40 +3,41 @@ import Account from "./Account/index";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import KambazNavigation from "./Navigation";
-import "./styles.css";
-import { useSelector } from "react-redux"; // ✅ Import Redux state
+import "./styles.css"
+
+// import { v4 as uuidv4 } from "uuid";
 import ProtectedRoute from "./Account/ProtectedRoute";
 
-export default function Kambaz() {
-  // ✅ Get courses dynamically from Redux
-  const courses = useSelector((state: any) => state.coursesReduccer.courses);
 
+export default function Kambaz() {
   return (
     <div id="wd-kambaz">
       <KambazNavigation />
       <div className="wd-main-content-offset p-3">
         <Routes>
-          <Route path="/" element={<Navigate to="/Kambaz/Account" />} />
+          <Route path="/" element={<Navigate to="Account" />} />
           <Route path="/Account/*" element={<Account />} />
-          
-          {/* ✅ Dashboard is now fully protected */}
-          <Route path="/Dashboard" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
-
-          {/* ✅ Courses now fetches courses from Redux automatically */}
-          <Route path="/Courses/:cid/*" element={
-            <ProtectedRoute>
-              <Courses />
-            </ProtectedRoute>
-          } />
-
+          <Route
+            path="/Dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/Navigation" element={<KambazNavigation />} />
+          <Route
+            path="/Courses/:cid/*"
+            element={
+              <ProtectedRoute>
+                <Courses />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/Calendar" element={<h1>Calendar</h1>} />
           <Route path="/Inbox" element={<h1>Inbox</h1>} />
         </Routes>
-      </div>   
+      </div>
     </div>
   );
 }
