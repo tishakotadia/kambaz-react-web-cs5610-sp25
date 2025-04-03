e2fb651f50c72ca5b2a36f62420c7f366bac9792
tishakotadia <tisha.kotadia@hotmail.com>
2025-04-03 18:39:22 -0400

    final changes frontend ass5

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -89,7 +89,7 @@ export default function Dashboard({
             className="float-end" 
             onClick={toggleShowAll}
           >
-            {showAll ? "Enrolled Courses" : "All Courses"}
+            {showAll ? "View Enrolled Courses" : "View All Courses"}
           </Button>
         )}
       </h1>
